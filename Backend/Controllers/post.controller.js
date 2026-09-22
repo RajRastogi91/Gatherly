7c1a68980f0d2e22d230c097a03d2443d91048b8
RajRastogi91 <136141418+RajRastogi91@users.noreply.github.com>
2026-09-22 23:13:39 +0530

    added more post actions

--- Backend/Controllers/post.controller.js
+++ Backend/Controllers/post.controller.js
@@ -158,3 +158,141 @@ export const dislikePost = async (req, res) => {
         });
     }
 };
+
+export const addComment = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const commentByUser = req.id;
+        const {text} = req.body;
+
+        const post = await Post.findyById(postId);
+        if(!text) return res.status(400).json({
+            message:"text is required.",
+            success: false
+        });
+
+        const comment = await Comment.create({
+            text,
+            author:commentByUser,
+            post:postId
+        }).populate({
+            path:'author',
+            select:'username, profilePicture'
+        });
+
+        post.comments.push(comment._id);
+        await post.save();
+
+        return res.status(201).json({
+            message:'comment added.',
+            comment,
+            success: true
+        })
+
+    } catch (error) {
+        console.error("addComment Error:", error);
+
+        return res.status(500).json({
+            message: "Internal server error.",
+            success: false
+        });
+    }
+};
+
+export const getPostComments = async(req, res) => {
+    try {
+        const postId = req.params.id;
+
+        const comments = await Comment.find({post:postId}).populate('author', 'username, profilePicture');
+
+        if(!comments) return res.status(400).json({message:'no comments found on this post.', success:flase});
+
+        return res.status(200).json({success:true, comments});
+    } catch (error) {
+        console.error("getPostComment Error:", error);
+
+        return res.status(500).json({
+            message: "Internal server error.",
+            success: false
+        });
+    }
+};
+
+export const deletePost = async (req, res) => {
+    try {
+        const postId = req. params.id;
+        const authorId = req.id;
+
+        const post = await Post.findById(postId);
+        if(!post) return res.status(404).json({
+            message:"post not found.",
+            success: false
+        });
+        // check if logged-in user is the owner of post
+        if(post.author.toString() !== authorId) return res.status(403).json({
+            message:"Unauthorize user."
+        });
+
+        await Post.findByIdAndDelete(postId);
+
+        // remove the post id from user's post
+        let user = await User.findById(authorId);
+        user.posts = user.posts.filter(id => id.toString() !== postId);
+        await user.save();
+
+        // delete associated comments
+        await Comments.deleteMany({post:postId});
+        return res.status(200).json({
+            message:"post deleted.",
+            success:true
+        })
+
+    } catch (error) {
+        console.error("deletePost Error:", error);
+
+        return res.status(500).json({
+            message: "Internal server error.",
+            success: false
+        });
+    }
+};
+
+export const bookmarkPost = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const authorId = req.id;
+        const post = await Post.findById(postId);
+        if(!post) return res.status(404).json({
+            message:"Post not found.",
+            success:false
+        });
+
+        const user = await User.findById(authorId);
+        if(user.bookmarks.includes(post._id)){
+            // already bookmarked
+            await user.updateOne({$pull:{bookmarks:post._id}});
+            await user.save();
+            return res.status(200).json({
+                type:'unsaved',
+                message:'Post removed successfully.',
+                success: true
+            });
+        } else{
+            // remove from bookmark
+            await user.updateOne({$addToSet:{bookmarks:post._id}});
+            await user.save();
+            return res.status(200).json({
+                type:'saved',
+                message:'post bookmarked',
+                success:true
+            });
+        }
+    } catch (error) {
+        console.error("bookmarkPost Error:", error);
+
+        return res.status(500).json({
+            message: "Internal server error.",
+            success: false
+        });
+    }
+}
